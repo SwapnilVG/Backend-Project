55581c1182605f759a5174ee68491400d03e03a2
SwapnilVG <swapnilgadekar2000@gmail.com>
2024-01-14 21:52:29 +0530

    config cloudinary

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 import app from "./app.js";
 import dbconnection from "./config/dbConnection.js";
+import cloudinary from 'cloudinary'
 const PORT = process.env.PORT || 8000
 
+cloudinary.v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
+})
 
 app.listen(PORT, async()=>{
     await dbconnection();
