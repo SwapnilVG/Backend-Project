f18f65e081a932f004082704697d20c2e6c01bf5
SwapnilVG <swapnilgadekar2000@gmail.com>
2024-01-14 15:30:47 +0530

    add upload.single

--- routers/user.routers.js
+++ routers/user.routers.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getProfile, login, logout, register } from "../controllers/user.controller.js";
 import { isLoggedIn } from "../middleware/auth.middleware.js";
+import upload from "../middleware/multer.middleware.js";
 
 const router = Router()
 
-router.post('/register',register)
+router.post('/register',upload.single("avatar"),register)
 router.post('/login',login)
 router.post('/logout',logout)
 router.get('/me',isLoggedIn,getProfile)
