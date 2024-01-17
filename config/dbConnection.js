586f7a42e3dab754b8f15166c7903a58b6cced3f
SwapnilVG <swapnilgadekar2000@gmail.com>
2024-01-17 16:58:50 +0530

    update dbConnection

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 mongoose.set('strictQuery',false)
 const dbconnection = async () =>{
     try {
-        const {connection} = await mongoose.connect(process.env.DB_URI || "mongodb://localhost:27017/Backend Project")
+        const {connection} = await mongoose.connect(process.env.DB_URI || "mongodb://127.0.0.1:27017/Backend_Project")
         if(connection){
             console.log(`Connected to MongoDB: ${connection.host}`)
         }
