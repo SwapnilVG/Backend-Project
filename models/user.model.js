e1d0bc1ef2f57a61b2984c4895110edc970168f7
SwapnilVG <swapnilgadekar2000@gmail.com>
2024-01-17 17:04:40 +0530

    add generatePasswordResetToken

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,6 @@
 import { Schema ,model } from "mongoose";
 import bcrypt from 'bcryptjs';
+import crypto from 'crypto';
 import JWT from 'jsonwebtoken'
 const userSchema = new Schema(
     {
@@ -74,6 +75,19 @@ userSchema.methods ={
 
     comparePassword: async function(plainPassword){
         return await bcrypt.compare(plainPassword,this.password)
+    },
+
+    generatePasswordResetToken: async function(){
+        const resetToken = crypto.randomBytes(20).toString('hex');
+        
+        this.forgotPasswordToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex')
+        
+        this.forgotPasswordExpiry = Date.now() * 15 * 60 * 1000; // 15min from now
+
+        return resetToken;
     }
 }
 
