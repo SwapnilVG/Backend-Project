c0856a52447ddbd3e9aa8dbda305ed395a04259f
SwapnilVG <swapnilgadekar2000@gmail.com>
2024-01-25 16:25:04 +0530

    add course routers

---
+++ routers/course.routers.js
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import { addLectureToCourseById, createCourse, getAllCourses, getLecturesByCourseId, removeCourse, removeLectureFromCourse, updateCourse } from "../controllers/course.controller.js";
+import upload from "../middleware/multer.middleware.js";
+import { authorizedRoles, isLoggedIn } from "../middleware/auth.middleware.js";
+
+const router = Router();
+// router.get('/',getAllCourses);
+
+router.route('/')
+   .get(getAllCourses)
+   .post(isLoggedIn,authorizedRoles('ADMIN'),upload.single('thumbnail'),createCourse)
+
+
+router.route('/:courseId/lectures/:lectureId')
+   .delete(isLoggedIn, authorizedRoles("ADMIN"), removeLectureFromCourse);
+
+   
+// router.get('/:id',getLecturesByCourseId);
+router.route('/:id')
+   .get(isLoggedIn,getLecturesByCourseId)
+   .put(isLoggedIn,authorizedRoles("ADMIN"),updateCourse)
+   .delete(isLoggedIn,authorizedRoles("ADMIN"),removeCourse)
+   .post(isLoggedIn,authorizedRoles("ADMIN"),upload.single('lecture'),addLectureToCourseById)
+   
+
+export default router
+
