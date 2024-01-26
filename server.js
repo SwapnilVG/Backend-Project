db86a902304498c844fb166fc342f57731fb5198
SwapnilVG <swapnilgadekar2000@gmail.com>
2024-01-26 20:55:21 +0530

    Initializing Razorpay

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ import app from "./app.js";
 import dbconnection from "./config/dbConnection.js";
 import cloudinary from 'cloudinary'
 const PORT = process.env.PORT || 8000
+import Razorpay from 'razorpay'
 
 cloudinary.v2.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key:process.env.CLOUDINARY_API_KEY,
     api_secret:process.env.CLOUDINARY_API_SECRET
+})
+
+export const razorpay = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_SECRET
 })
 
 app.listen(PORT, async()=>{
