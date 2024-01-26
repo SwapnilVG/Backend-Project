33040e4e0b1367513d1c72ea5af730c48608878f
SwapnilVG <swapnilgadekar2000@gmail.com>
2024-01-26 21:20:29 +0530

    add payment routers

---
+++ routers/payment.routers.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import { allPayments, buySubscription, cancelSubscription, getRazorpayApiKey, verifySubscription } from "../controllers/payment.controller.js";
+import { authorizedRoles, isLoggedIn } from "../middleware/auth.middleware.js";
+const router = Router();
+
+router.route('/razorpay-key')
+    .get(isLoggedIn,getRazorpayApiKey);
+
+router.route('/subscribe')
+    .post(isLoggedIn,buySubscription)
+    
+router.route('/verify')
+    .post(isLoggedIn,verifySubscription)
+
+router.route('/unsubscribe')
+    .post(isLoggedIn,cancelSubscription)    
+
+router.route('/') 
+    .get(isLoggedIn,authorizedRoles('ADMIN'),allPayments)
+
+
+export default router    
