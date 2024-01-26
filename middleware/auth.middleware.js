b911fcda5a878ffadd89b569063fa57c024e3b03
SwapnilVG <swapnilgadekar2000@gmail.com>
2024-01-26 20:59:29 +0530

    add authorizeSubscriber middleware

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -27,6 +27,21 @@ const authorizedRoles = (...roles) => async (req,res,next)=>{
     next()
 }
 
+
+
+const authorizeSubscriber = async(req,res,next)=>{
+    const subscription = req.user.subscription;
+    const currentUserRole = req.user.role;
+
+    if(currentUserRole !=='ADMIN' && subscription.status !== 'active'){
+        return next(new AppError('Please subscribe to access this route!',403))
+    }
+
+    next()
+}
+
+
+
 export {
-    isLoggedIn,authorizedRoles
+    isLoggedIn,authorizedRoles,authorizeSubscriber
 }
